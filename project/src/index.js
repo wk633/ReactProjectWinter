120462e56adf558907288e65781e409f24741bb4
wk633 <wk633@outlook.com>
2017-12-13 12:14:23 -0800

    use chrome redux dev tool extension

--- project/src/index.js
+++ project/src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { count } from './myRedux';
 
+const reduxDevTools = window.devToolsExtension?window.devToolsExtension():f=>f;
 
-const store = createStore(count, applyMiddleware(thunk));
+
+const store = createStore(count, compose(applyMiddleware(thunk), reduxDevTools));
 
 function render(){
     ReactDOM.render(<App store={store}/>, document.getElementById('root'));
