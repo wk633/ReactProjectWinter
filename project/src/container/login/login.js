507da6bd7145948b8ca7df3cbaf9482ed35560e3
wk633 <wk633@outlook.com>
2017-12-15 12:39:45 -0800

    login page sign up button redirection

--- project/src/container/login/login.js
+++ project/src/container/login/login.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Logo from '../../component/logo/logo';
+import {List, InputItem, WingBlank, WhiteSpace, Button} from 'antd-mobile';
 
 class Login extends React.Component{
+    constructor(props) {
+        super(props);
+        this.register = this.register.bind(this);
+    }
+    register(){
+        this.props.history.push('/register');
+    }
     render(){
         return (
             <div>
                 <Logo></Logo>
-                <h2>login page</h2>      
+                <WingBlank>
+                    <Button type="primary">Log In</Button>
+                    <WhiteSpace/>
+                    <Button onClick={this.register} type="primary">Sign Up</Button>
+                </WingBlank>  
             </div>
                   
         );
--- project/src/container/register/register.js
+++ project/src/container/register/register.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Logo from '../../component/logo/logo';
 
+
 class Register extends React.Component{
+    
     render(){
         return (
             <div>
                 <Logo></Logo>
-                <h2>register page</h2>
             </div>
         );
     }
