af1ec648972071b0d17708fb212b6e5f4baafdf4
wk633 <wk633@outlook.com>
2017-12-14 19:32:44 -0800

    comment log output

--- project/src/Dashboard.js
+++ project/src/Dashboard.js
@@ -24,7 +24,6 @@ class Test extends React.Component {
 )
 class Dashboard extends React.Component{
     render(){
-        console.log(this.props);
         const match = this.props.match;
         const redirectToLogin = (<Redirect to='/login'></Redirect>);
         const app = (
