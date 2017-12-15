fcb8e996e97e719b600b6c58ecd094d960ce4c76
wk633 <wk633@outlook.com>
2017-12-15 12:08:17 -0800

    add logo component

---
+++ project/src/component/logo/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logoImg from './joblogo.png';
+import './logo.css';
+
+export default class Logo extends React.Component{
+    render(){
+        return (
+            <div className="logo-container">
+                <img src={logoImg} />
+            </div>
+        )
+    }
+}
--- project/src/container/login/login.js
+++ project/src/container/login/login.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Logo from '../../component/logo/logo';
 
 class Login extends React.Component{
     render(){
-        return <h2>login page</h2>;
+        return (
+            <div>
+                <Logo></Logo>
+                <h2>login page</h2>      
+            </div>
+                  
+        );
     }
 }
 export default Login;
